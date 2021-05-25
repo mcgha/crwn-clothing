85de2cc3866c2731e0bf7c1a0cd2e8853a1e6e6f
Marcos Campos <marcoscampos@Marcoss-MacBook-Pro.local>
2021-05-25 16:52:30 -0400

    updating app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,43 +3,45 @@ import { Route, Switch, Link } from 'react-router-dom';
 // swtich wraps route, route wraps
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import ShopPage from './pages/shop/shop.component';
 
-const HomePageNav = (props) => {
+// const HomePageNav = (props) => {
 
-  return(
-    <div>
-      {/* <Link to='/topics'>Topics</Link> */}
-      <button onClick={() => props.history.push('/topics')}>Topics</button>
-      <HomePage />
-    </div>
-  );
-};
+//   return(
+//     <div>
+//       {/* <Link to='/topics'>Topics</Link> */}
+//       <button onClick={() => props.history.push('/Shop')}>Shop</button>
+//       <HomePage />
+//     </div>
+//   );
+// };
 
-const TopicList = () => {
-  return ( 
-    <div>
-      <Link to='/'>Home</Link>
-      <h1>Topic List</h1>
-    </div>
-  );
-  };
+// const TopicList = () => {
+//   return ( 
+//     <div>
+//       <Link to='/'>Home</Link>
+//       <h1>Topic List</h1>
+//     </div>
+//   );
+//   };
 
-  const TopicDetail = (props) => {
-    return ( 
-      <div>
-        <h1>TopicDetail Page: {props.match.params.topicId}</h1>
-      </div>
-    );
-    };
+//   const TopicDetail = (props) => {
+//     return ( 
+//       <div>
+//         <h1>TopicDetail Page: {props.match.params.topicId}</h1>
+//       </div>
+//     );
+//     };
 
 function App() {
   return (
     <div className="App">
       {/* <HomePage /> */}
       <Switch>
-        <Route exact path='/' component={HomePageNav} />
-        <Route exact path='/topics' component={TopicList} />
-        <Route path='/topics/:topicId' component={TopicDetail} />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        {/* <Route exact path='/topics' component={TopicList} /> */}
+        {/* <Route path='/topics/:topicId' component={TopicDetail} /> */}
       </Switch> 
     </div>
   );
