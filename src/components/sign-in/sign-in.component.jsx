c75d5f1696eb13ea3a1746b4bbafb5a1a0cbfd63
Marcos Campos <marcoscampos@Marcoss-MacBook-Pro.local>
2021-05-29 20:54:36 -0400

    minor fixes before implementing redux

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,7 +54,7 @@ class SignIn extends Component {
                         required/>
                     <div className='buttons'>
                         <CustomButton type='submit'> Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In with Google</CustomButton>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn> Sign In with Google</CustomButton>
                     </div>
                     
                 </form>
