31e289b80d8a1f1d0e180f11ea166d711ee762b2
Marcos Campos <marcoscampos@Marcoss-MacBook-Pro.local>
2021-06-16 21:01:57 -0400

    removed empty contact page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -24,9 +24,9 @@ const Header = ({ currentUser, hidden }) => (
             <Link className='option' to='/shop'>
                 Shop
             </Link>
-            <Link className='option' to='/contact'>
+            {/* <Link className='option' to='/contact'>
                 Contact
-            </Link>
+            </Link> */}
             {
                 currentUser ? 
                 <div className='option' onClick={() => auth.signOut()}> Sign out ({currentUser.displayName})</div>   
